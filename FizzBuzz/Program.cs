a4068340183d47b60e214dba736d0588d049e04f
Rob <Rmilla445@gmail.com>
2022-11-15 14:33:43 -0500

    fizzbuzz

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace FizzBuzz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public string FizzBuzz(int number)
+        {
+            var answer = "";
+            if (number % 3 == 0)
+            {
+                answer = "fizz";
+            }
+            if (number % 5 == 0)
+            {
+                answer += "buzz";
+            }
+
+            return answer;
+        }
+    }
+}
